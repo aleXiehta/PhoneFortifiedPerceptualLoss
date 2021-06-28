4db1901f72acbc394c629b0852b196e892a2e8fc
aleXiehta <r05944036@ntu.edu.tw>
2021-06-28 16:46:35 +0800

    Add Wasserstein distance to perceptual loss

--- perceptual_loss/losses.py
+++ perceptual_loss/losses.py
@@ -5,12 +5,13 @@ import torch.nn.functional as F
 from fairseq.models.wav2vec import Wav2VecModel
 import os
 from functools import partial
-
+from geomloss import SamplesLoss
 
 class PerceptualLoss(nn.Module):
     def __init__(self, model_type='wav2vec', PRETRAINED_MODEL_PATH = '/path/to/wav2vec_large.pt'):
         super().__init__()
         self.model_type = model_type
+        self.wass_dist = SamplesLoss()
         if model_type == 'wav2vec':
             ckpt = torch.load(PRETRAINED_MODEL_PATH)
             self.model = Wav2VecModel.build_model(ckpt['args'], task=None)
@@ -23,4 +24,6 @@ class PerceptualLoss(nn.Module):
 
     def forward(self, y_hat, y):
         y_hat, y = map(self.model, [y_hat, y])
-        return torch.abs(y_hat - y).mean()
+        return self.wass_dist(y_hat, y)
+        # for PFPL-W-MAE or PFPL-W
+        # return torch.abs(y_hat - y).mean()
